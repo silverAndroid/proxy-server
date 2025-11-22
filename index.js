27dee208da03525e49fc935bae35bb3f0cb34f9b
Rushil Perera <pererarushil1940@gmail.com>
2025-11-22 09:45:21 +0700

    feat: Add Hono logger middleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 
 const app = new Hono()
+app.use(logger());
 
 app.post('/proxy', async (c) => {
     try {
