7849d86b01c615657fbe9938ea13ea323fddf6f4
Rushil Perera <pererarushil1940@gmail.com>
2025-12-19 09:33:47 -0500

    feat: Upgrade Node.js to v20 and integrate OpenTelemetry for tracing and logging.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:18-alpine
+FROM node:20-alpine
 
 WORKDIR /app
 
@@ -7,6 +7,12 @@ COPY package.json package-lock.json* ./
 RUN npm install
 
 COPY . .
+
+ENV OTEL_EXPORTER_OTLP_ENDPOINT="https://ingest.otel.rururu.dev"
+ENV OTEL_NODE_RESOURCE_DETECTORS="env,host,os"
+ENV OTEL_SERVICE_NAME="proxy-server"
+ENV OTEL_TRACES_EXPORTER="otlp"
+ENV NODE_OPTIONS="--require @opentelemetry/auto-instrumentations-node/register"
 
 EXPOSE 3000
 
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { requestId } from 'hono/request-id';
 import { pinoLogger } from 'hono-pino'
 import pino from "pino";
 import { ulid } from "ulid";
+import { trace } from "@opentelemetry/api";
 
 const app = new Hono();
 app.use('*', requestId({ generator: () => ulid() }));
@@ -11,7 +12,19 @@ app.use(
   pinoLogger({
     pino: pino({
       transport: {
-        target: "hono-pino/debug-log",
+        targets: [
+          {
+            target: 'hono-pino/debug-log',
+          },
+          {
+            target: 'pino-opentelemetry-transport',
+            options: {
+              logRecordProcessorOptions: {
+                processor: 'simple',
+              }
+            }
+          },
+        ],
       },
     }),
   }),
@@ -23,43 +36,55 @@ app.post("/proxy", async (c) => {
   let response;
   const requestId = c.get('requestId');
   const { logger } = c.var;
-  try {
-    const body = await c.req.json();
-    const { url, method = "GET", headers = {}, data } = body;
+  const tracer = trace.getTracer(process.env.OTEL_SERVICE_NAME);
 
-    if (!url) {
-      return c.json({ error: "URL is required" }, 400);
+  return tracer.startActiveSpan('proxy', async (span) => {
+    try {
+      const body = await c.req.json();
+      const { url, method = "GET", headers = {}, data } = body;
+      span.setAttribute('requestId', requestId);
+      span.setAttribute('domain', new URL(url).hostname);
+      span.setAttribute('url', url);
+      span.setAttribute('method', method);
+
+      if (!url) {
+        span.recordException(new Error("URL is required"));
+        return c.json({ error: "URL is required" }, 400);
+      }
+
+      const fetchOptions = {
+        method,
+        headers,
+        body: data ? JSON.stringify(data) : undefined,
+      };
+
+      response = await fetch(url, fetchOptions);
+      const contentType = response.headers.get("content-type");
+      let parsedData;
+
+      if (contentType && contentType.includes("json")) {
+        parsedData = await response.json();
+      } else {
+        parsedData = await response.text();
+      }
+
+      return c.json(
+        {
+          status: response.status,
+          statusText: response.statusText,
+          headers: Object.fromEntries(response.headers.entries()),
+          data: parsedData,
+        },
+        response.status,
+      );
+    } catch (error) {
+      span.recordException(new Error('Failed to proxy request', error));
+      logger.error(`Failed to proxy request ${requestId}`, error);
+      return c.json({ error: error.message }, 500);
+    } finally {
+      span.end();
     }
-
-    const fetchOptions = {
-      method,
-      headers,
-      body: data ? JSON.stringify(data) : undefined,
-    };
-
-    response = await fetch(url, fetchOptions);
-    const contentType = response.headers.get("content-type");
-    let parsedData;
-
-    if (contentType && contentType.includes("json")) {
-      parsedData = await response.json();
-    } else {
-      parsedData = await response.text();
-    }
-
-    return c.json(
-      {
-        status: response.status,
-        statusText: response.statusText,
-        headers: Object.fromEntries(response.headers.entries()),
-        data: parsedData,
-      },
-      response.status,
-    );
-  } catch (error) {
-    logger.error(`Failed to proxy request ${requestId}`, error);
-    return c.json({ error: error.message }, 500);
-  }
+  });
 });
 
 const port = 3000;
